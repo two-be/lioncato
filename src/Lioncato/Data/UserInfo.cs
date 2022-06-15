6824f775c97b663f4b4b849c19ab409c4c995cc0
Two <Two.Logician@icloud.com>
2022-06-15 09:36:02 +0700

    :bug:

--- src/Lioncato/Data/UserInfo.cs
+++ src/Lioncato/Data/UserInfo.cs
@@ -1,6 +1,6 @@
 namespace Lioncato.Data;
 
-public class UserInfo
+public record UserInfo
 {
     public string Id { get; set; } = Guid.NewGuid().ToString("N");
     public string Password { get; set; } = string.Empty;
